874f9dc53648c9c3c99cb52ec806bfeee3392ae7
AdilZd <nowayat0me@gmail.com>
2023-01-20 15:20:36 +0100

    fonction pr turn les cartes

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,25 @@
 const cards = document.querySelectorAll(".card")
 
 function randomCartes(){
-  cards.forEach(card => {
-    const randomPos = Math.trunc(Math.random() * 12)
-    card.style.order = randomPos;
-  })
+    cards.forEach(card => {
+        let randomPos = Math.trunc(Math.random() * 8)
+        card.style.order = randomPos;
+    })
 }
 randomCartes()
+
+
+
+cards.forEach(card => card.addEventListener("click", tournerCarte))
+
+let lockedCards = false;
+let cardsPicked = []
+function tournerCarte(e){
+
+    if(lockedCards) return;
+saveCard(e.target.children[0], e.target.getAttribute("data-attr"))
+
+    if(cardsPicked.length === 2) result()
+}
+
+
