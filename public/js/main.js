8fb8ae75dd2b914c798d0474f76216ff42fe44d8
AdilZd <nowayat0me@gmail.com>
2023-01-22 15:26:31 +0100

    modification encore

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,31 @@
 
+
 let cards = document.querySelectorAll(".card");
 let compteurScore = document.querySelector(".score");
-let score = 0      ;
+let score = 0;
+let cartesRetournees = [];
 
 cards.forEach(card => {
     card.addEventListener("click", function() {
-        if (card.classList.contains("flipped")) {
-            card.classList.remove("flipped");
-        } else {
+        if (!card.classList.contains("flipped")) {
+            cartesRetournees.push(card);
             card.classList.add("flipped");
             score++;
             compteurScore.textContent = "Nombre de coups : " + score;
         }
+
+        if (cartesRetournees.length === 2) {
+            if (cartesRetournees[0].getAttribute("data-attr") === cartesRetournees[1].getAttribute("data-attr")) {
+                cartesRetournees[0].classList.add("matched");
+                cartesRetournees[1].classList.add("matched");
+                cartesRetournees = [];
+            } else {
+                setTimeout(function() {
+                    cartesRetournees[0].classList.remove("flipped");
+                    cartesRetournees[1].classList.remove("flipped");
+                    cartesRetournees = [];
+                }, 600);
+            }
+        }
     });
 });
-
