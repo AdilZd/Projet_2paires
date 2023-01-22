0c7754dadf64e50a23f90d2ca29590de141ecb0d
AdilZd <nowayat0me@gmail.com>
2023-01-22 15:20:26 +0100

    modification

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,17 @@
 
-const cards = document.querySelectorAll(".card")
+let cards = document.querySelectorAll(".card");
+let compteurScore = document.querySelector(".score");
+let score = 0      ;
 
-function randomCartes(){
-    cards.forEach(card => {
-        let randomPos = Math.trunc(Math.random() * 8)
-        card.style.order = randomPos;
-    })
-}
-randomCartes()
-
-
-
-cards.forEach(card => card.addEventListener("click", tournerCarte))
-
-let lockedCards = false;
-let cardsPicked = []
-function tournerCarte(e){
-
-    if(lockedCards) return;
-saveCard(e.target.children[0], e.target.getAttribute("data-attr"))
-
-    if(cardsPicked.length === 2) result()
-}
-
+cards.forEach(card => {
+    card.addEventListener("click", function() {
+        if (card.classList.contains("flipped")) {
+            card.classList.remove("flipped");
+        } else {
+            card.classList.add("flipped");
+            score++;
+            compteurScore.textContent = "Nombre de coups : " + score;
+        }
+    });
+});
 
